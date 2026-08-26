a74b112aafd3ab29651019d6683690755cbdbaea
Jim Endler <jim.endler@broadcom.net>
2026-08-26 15:30:33 -0400

    new sql

--- Content/db copy.sql
+++ Content/db copy.sql
@@ -1,17 +1,19 @@
-SELECT CURRENT SERVER FROM SYSIBM.SYSDUMMY1;
-
-SELECT CURRENT SERVER
-      ,DBNAME
-      ,SUBSTR(CREATOR,1,8)        AS CREATOR
-      ,SUBSTR(NAME,1,18)          AS NAME
-  FROM SYSIBM.SYSTABLES
- WHERE CREATOR = 'DEV001'
-   AND NAME    = 'ORDERLINES' ;   
-
-SELECT CURRENT SERVER
-      ,COLNO
-      ,SUBSTR(name,1,15)            AS COLNAME
-  FROM SYSIBM.SYSCOLUMNS
- WHERE TBCREATOR = 'DEV001'
-   AND TBNAME    = 'ORDERLINES' 
- ORDER BY COLNO;                  
+SELECT  CURRENT SERVER AS SUBSYSTEM, DBNAME , TSNAME           
+        , SUBSTR ( NAME , 1 , 18) AS TBNAME        
+        , SUBSTR ( CREATOR , 1 , 8 ) AS CREATOR    
+   FROM SYSIBM.SYSTABLES                           
+  WHERE CREATOR = 'INT001'                         
+    AND NAME = 'ORDERLINES'                        
+  ORDER BY NAME , CREATOR WITH CS          
+;
+SELECT A.COLNO   
+      , A.NAME AS COLNAME                        
+      , A.COLTYPE                               
+      , A.LENGTH                                
+ FROM SYSIBM.SYSCOLUMNS A , SYSIBM.SYSTABLES B  
+WHERE ( A.TBCREATOR = B.CREATOR                 
+  AND A.TBNAME = B.NAME )                       
+  AND A.TBCREATOR = 'INT001'                    
+  AND A.TBNAME = 'ORDERLINES'                   
+ORDER BY A.COLNO WITH CS                        
+;
