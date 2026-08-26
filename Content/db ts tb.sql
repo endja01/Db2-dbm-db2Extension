a9194d91255b9caf9e0ba41287aa20cb56aa12c2
Jim Endler <jim.endler@broadcom.net>
2026-08-26 16:00:31 -0400

    ADD IX

--- Content/db ts tb.sql
+++ Content/db ts tb.sql
@@ -2,7 +2,7 @@ SELECT  CURRENT SERVER AS SUBSYSTEM, DBNAME , TSNAME
         , SUBSTR ( NAME , 1 , 18) AS TBNAME        
         , SUBSTR ( CREATOR , 1 , 8 ) AS CREATOR    
    FROM SYSIBM.SYSTABLES                           
-  WHERE CREATOR = 'INT001'                         
+  WHERE CREATOR = 'DEV016'                         
     AND NAME = 'ORDERLINES'                        
   ORDER BY NAME , CREATOR WITH CS          
 ;
@@ -13,7 +13,16 @@ SELECT A.COLNO
  FROM SYSIBM.SYSCOLUMNS A , SYSIBM.SYSTABLES B  
 WHERE ( A.TBCREATOR = B.CREATOR                 
   AND A.TBNAME = B.NAME )                       
-  AND A.TBCREATOR = 'INT001'                    
+  AND A.TBCREATOR = 'DEV016'                    
   AND A.TBNAME = 'ORDERLINES'                   
 ORDER BY A.COLNO WITH CS                        
 ;
+SELECT SUBSTR ( NAME , 1 , 18) AS IXNAME        
+      , SUBSTR ( CREATOR , 1 , 8 ) AS IXCREATOR  
+      , SUBSTR (TBNAME, 1, 18) AS TBNAME
+      , COLCOUNT
+      , UNIQUERULE 
+   FROM SYSIBM.SYSINDEXES                           
+  WHERE CREATOR = 'DEV016'                         
+    AND NAME = 'ORDERLINES_IX1'                       
+  ORDER BY NAME , CREATOR WITH CS  
